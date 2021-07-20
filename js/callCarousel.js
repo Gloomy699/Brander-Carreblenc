e067388bc4947bf1cb524749576d5f1d51eea073
Yaroslav Vasylenko <gloomy.699@gmail.com>
2021-07-21 02:36:51 +0300

    complete carousel

---
+++ js/callCarousel.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    const slider = $("#slider").owlCarousel({
+      items: 5,
+        loop: true,
+      nav: true,
+        margin:10,
+        navText: [
+          '<svg width="35" height="35" viewBox="0 0 24 24"><path d="M16.67 0l2.83 2.829-9.339 9.175 9.339 9.167-2.83 2.829-12.17-11.996z"/></svg>',
+          '<svg width="35" height="35" viewBox="0 0 24 24"><path d="M5 3l3.057-3 11.943 12-11.943 12-3.057-3 9-9z"/></svg>'
+        ]
+     });
+});
